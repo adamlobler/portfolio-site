904f4ccbaefe3300548821f5e60ac6246c788f7c
Lobli <lobli@icloud.com>
2023-01-16 14:59:53 +0100

    Theme customization with colors

--- components/button.tsx
+++ components/button.tsx
@@ -9,8 +9,8 @@ const Button: React.FC<Props> = (props) => {
   const { type = "primary", text } = props;
   const className =
     type === "primary"
-      ? "bg-blue-500 text-button hover:bg-blue-700 text-white py-4 px-6 rounded-sm"
-      : "bg-transparent text-button hover:bg-gray-100 py-4 px-6 border border-black hover:border-transparent rounded-sm";
+      ? "bg-primary-500 text-button  hover:bg-primary-600 text-white py-4 px-6 rounded-sm"
+      : "bg-transparent text-button hover:bg-gray-50 py-4 px-6 border border-black hover:border-transparent rounded-sm";
   return <button className={`${className}`}>{text || "Default"}</button>;
 };
 
