1571f297d27d796107182cd80bfe87de981aa745
Lobli <lobli@icloud.com>
2023-01-19 17:38:39 +0100

    Fix mobile layout

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,9 +4,9 @@ import Socials from "./Socials";
 const Footer: React.FC = () => {
   return (
     <footer className="flex flex-col w-full bg-gray-50 dark:bg-gray-900 items-center justify-center ">
-      <div className="flex max-w-2xl xl:max-w-7xl flex-col xl:flex-row w-full space-y-16 xl:items-center justify-between px-4 py-32">
+      <div className="flex max-w-2xl xl:max-w-7xl flex-col xl:flex-row w-full space-y-16 xl:items-center justify-between px-4 py-16 md:py-32">
         <div className="flex flex-col items-start space-y-8">
-          <h2 className="text-h2 dark:text-white">
+          <h2 className="text-h4 xl:text-h2 dark:text-white">
             Looking for help with{" "}
             <span className=" text-primary-500 dark:text-primary-400">
               design?
