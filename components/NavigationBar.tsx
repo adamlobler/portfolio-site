ceb94a2341775a1fa821a558af0569f523fd53ad
Lobli <lobli@icloud.com>
2023-03-21 11:36:39 +0100

    Fix navbar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -29,7 +29,7 @@ const NavigationBar: React.FC = () => {
       : (document.body.style.overflow = "hidden");
   };
   return (
-    <nav className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed overflow-hidden w-full top-0 left-0 max-h-screen z-50">
+    <nav className="bg-white pl-4 px-2 sm:px-4 py-2 dark:bg-gray-900 sticky overflow-hidden w-full top-0 left-0 max-h-screen z-50">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link href="/" className="flex items-center">
           <span className="text-button font-semibold whitespace-nowrap dark:text-white">
@@ -40,7 +40,7 @@ const NavigationBar: React.FC = () => {
             </span>
           </span>
         </Link>
-        <div className="flex">
+        <div className="flex justify-center items-center">
           {/*Desktop navbar items*/}
           <div className="items-center justify-between hidden w-full md:flex md:w-auto mr-8">
             <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-900 md:dark:bg-gray-900 dark:border-gray-700">
@@ -57,10 +57,10 @@ const NavigationBar: React.FC = () => {
               ))}
             </ul>
           </div>
-          <div className="flex">
+          <div className="flex justify-center items-center">
             <ThemeSwitch />
             <Link
-              className="btn-primary h-14 hidden md:block"
+              className="btn-primary hidden md:block"
               href="mailto:adam.lobler@vallio.studio"
             >
               Hire me!
@@ -69,7 +69,7 @@ const NavigationBar: React.FC = () => {
               onClick={() => toggleNavbar()}
               data-collapse-toggle="navbar-sticky"
               type="button"
-              className="inline-flex items-center p-2 text-sm text-primary-500 dark:text-primary-400 rounded-sm md:hidden hover:bg-primary-50 focus:outline-none focus:ring-2 focus:ring-gray-20 dark:hover:bg-gray-900 dark:focus:ring-gray-800 ml-2"
+              className="inline-flex items-center p-2 text-sm text-primary-500 dark:text-primary-400 rounded-sm md:hidden hover:bg-primary-50 dark:hover:bg-primary-800 focus:outline-none focus:ring-2 focus:ring-gray-20 dark:focus:ring-gray-800 ml-2"
               aria-controls="navbar-sticky"
               aria-expanded="false"
               title="Menu button"
@@ -82,7 +82,7 @@ const NavigationBar: React.FC = () => {
       <div
         className={`${
           isOpen
-            ? "flex md:hidden overflow-hidden flex-col items-center justify-center slide-in w-full h-screen space-y-8 pb-12"
+            ? "flex md:hidden overflow-hidden flex-col items-center justify-center slide-in w-full h-screen space-y-8 px-4 pb-12"
             : "hidden"
         }`}
         id="navbar-sticky"
@@ -93,23 +93,25 @@ const NavigationBar: React.FC = () => {
             <li key={index}>
               <Link
                 href={item.link}
-                className="text-subtitle1 text-center block py-2 pl-3 pr-4 text-black rounded md:bg-transparent md:text-black dark:text-white hover:text-primary-500 md:p-0"
+                className="text-subtitle1 text-center block py-6 text-black  md:bg-transparent dark:text-white md:p-0"
                 aria-current="page"
+                onClick={() => toggleNavbar()}
               >
                 {item.name}
               </Link>
             </li>
           ))}
         </ul>
+
+        <div className="pt-14">
+          <Socials />
+        </div>
         <a
-          className="btn-primary w-72 text-center h-14"
+          className="btn-primary w-full mx-8"
           href="mailto:adam.lobler@vallio.studio"
         >
           Hire me!
         </a>
-        <div className="pt-14">
-          <Socials />
-        </div>
       </div>
     </nav>
   );
