66c506a28d02bdea126c763fa682dea3131a9b4e
Adam Lobler <lobler.adam@gmail.com>
2024-11-19 15:41:09 +0100

    try og image

--- pages/works/[id].tsx
+++ pages/works/[id].tsx
@@ -90,7 +90,7 @@ const WorksDetailPage = ({
         <meta name="description" content={project.description} key="desc" />
         <meta property="og:title" content={"Adam Lobler | " + project.title} />
         <meta property="og:description" content={project.description} />
-        <meta property="og:image" content="/img/og_image.png" />
+        <meta property="og:image" content="/img/airtime_desktop.png" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Lightbox
